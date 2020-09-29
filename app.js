7f30ff0f8254b8e3e5b2e109c299e19611b0702e
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-29 17:22:55 -0400

    rebecca

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/home", (req, res, next) => {
+  res.sendfile(__dirname + "/views/home.html");
+});
+app.get("/about", (req, res, next) => {
+  res.sendfile(__dirname + "/views/about.html");
+});
+app.get("/works", (req, res, next) => {
+  res.sendfile(__dirname + "/views/works.html");
+});
 
-
-app.listen(3000, () => console.log('Test test, this is homebase on 3000'))
+app.listen(3000, () => console.log("Test test, this is homebase on 3000"));
